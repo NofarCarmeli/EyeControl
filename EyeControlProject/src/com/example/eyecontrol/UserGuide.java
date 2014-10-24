ec12565dac06b913f866daecad8113ebf9c9b91c
NofarCarmeli <carmelinofar@gmail.com>
2014-10-24 10:51:28 +0300

    remove unused menus and settings option

---
+++ EyeControlProject/src/com/example/eyecontrol/UserGuide.java
@@ -0,0 +1,16 @@
+package com.example.eyecontrol;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class UserGuide extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_user_guide);
+	}
+
+}
